00c59dcc9fde467f9d7d2b59ae80b2c1c6fe2bf9
user.name <gmail@hdknr.com>
2022-12-17 15:45:30 +0900

    fields mapper

--- php-mailer/conf.php
+++ php-mailer/conf.php
@@ -1,6 +1,16 @@
 <?php
 $domain = "mysite.com";
 
+$meta = array(
+    "fields" => array(
+        "id" => "formId",
+        "recaptcha" => "reCAPTCHA",
+        "csrftoken" => "csrfToken",
+        "subject"  => "subject",
+        "body" => "body",
+        "email" => "email",
+    ),
+);
 $conf = array(
     "default" =>  array(
         "email_from" => "no-reply@{$domain}",
