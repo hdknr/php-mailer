cb7c31ca479f087cfc697349f65b3ddb29daf1bb
user.name <gmail@hdknr.com>
2022-12-18 17:28:07 +0900

    about configuration

--- php-mailer/conf.php
+++ php-mailer/conf.php
@@ -4,6 +4,7 @@ $domain = "mysite.com";
 $recaptcha_secretkey = getenv('RECAPTCHA_SECRETKEY');
 
 $meta = array(
+    # POST parameter name mapping
     "fields" => array(
         "id" => "formId",
         "recaptcha" => "g-recaptcha-response",
@@ -17,9 +18,14 @@ $meta = array(
     )
 );
 
+# configuration for each form ID
 $conf = array(
     "default" =>  array(
         "email_from" => "no-reply@{$domain}",
         "email_to" => "contact@{$domain}",
+    ),
+    "form-id-1" =>  array(
+        "email_from" => "no-reply-1@{$domain}",
+        "email_to" => "contact-1@{$domain}",
     ),
 );
